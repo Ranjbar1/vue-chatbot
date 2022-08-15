130c2bad3728c14c680885908353d1caadaba026
Abolfazl (Naser) Ranjbar <34566049+Ranjbar1@users.noreply.github.com>
2022-08-15 13:33:54 +0430

    improve style

--- vue-chatbot/vue-chatbot/src/App.vue
+++ vue-chatbot/vue-chatbot/src/App.vue
@@ -64,17 +64,26 @@ export default {
   align-items: center;
 }
 input {
-  width: 50%;
+  min-width: 300px;
+  height: 30px;
+
+  background-color: rgb(178, 253, 253);
+  border: 2px solid rgb(17, 67, 231);
+  border-radius: 5px;
+  padding: 5px;
+  color: black;
 }
 button {
   all: unset;
-  border: 1px solid #ccc;
+  border: 2px solid rgb(17, 67, 231);
+  border-radius: 20px;
   padding: 0.5em 1em;
   margin: 0.5em;
 
   margin-top: 10px;
 }
 ul {
+  color: hsla(160, 100%, 37%, 1);
   list-style: none;
   padding: 0;
   margin: 0;
